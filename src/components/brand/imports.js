633ddce3b184bfe5911ad5e823b80ceb718e57c9
Microvolts1 <vuivatva305@gmail.com>
2023-03-14 13:01:39 +0700

    first commit

---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from "../../assets/assets1/google.png";
+import slack from "../../assets/assets1/slack.png";
+import atlassian from "../../assets/assets1/atlassian.png";
+import dropbox from "../../assets/assets1/dropbox.png";
+import shopify from "../../assets/assets1/shopify.png";
+
+export {
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify
+}
